1e8a78c01f3eeb9453198d1ae298166ac89d79ab
Rahul Pandey <rahulpandey.rp1996@gmail.com>
2019-02-01 12:15:59 +0530

    Arduino code for Motion Sensor

---
+++ motion/Readme.md
@@ -0,0 +1,14 @@
+Program for Motion Sensor Using Arduinom Studio.
+
+for this Program we need------
+1:-Arduino uno board
+2:-Zumper wires Make to Female
+3:-Motion Sensor
+
+
+How To upload code on Arduino board---
+1:-First Download Arduino Studio from
+        arduino.cc/downloads
+2:- And install and open the arduino studio
+3:- And then set the port for arduino board like Com1,com3 etc from the tools menu.
+4:-And paste the given code on the arduino studio and then verify and upload the code on the arduino board
---
+++ motion/motion.ino
@@ -0,0 +1,34 @@
+int ledPin=11;
+int inputPin=2;
+int pirState=LOW;
+int val=0;
+void setup() {
+  pinMode(ledPin,OUTPUT);
+  pinMode(inputPin,INPUT);
+  Serial.begin(9600);
+  }
+
+void loop() {
+  val=digitalRead(inputPin);
+  if(val==HIGH)
+  {
+    digitalWrite(ledPin,HIGH);
+    if(pirState==LOW)
+    {
+    Serial.println("Moion detected");
+    pirState=HIGH;
+    
+    }
+  }
+  else {
+    digitalWrite(ledPin,LOW);
+    if(pirState==HIGH)
+    {
+    Serial.println("Moion ended");
+    pirState=LOW;
+    
+    }
+    
+  }
+ 
+}
